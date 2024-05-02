5a737f1ac22eeed8dd450be9334bd60acb2e28cf
Ben Hoyle <benjhoyle@gmail.com>
2024-05-02 15:53:16 +0100

    Refactor helper_functions.py and update project settings in .idea
    This commit refactors the `helper_functions.py` in `streaming_chat/config` to improve readability and maintainability. In addition, it updates SDK options, ruff executable paths, and formatting settings in .idea project settings. It also adds init files to several packages.

---
+++ streaming_chat/__init__.py
@@ -0,0 +1,1 @@
+"""Init for streaming_chat package."""
--- streaming_chat/config/helper_functions.py
+++ streaming_chat/config/helper_functions.py
@@ -1,30 +1,28 @@
 """Configuration helper functions."""
 
+from __future__ import annotations
+
+import contextlib
 import os
+from pathlib import Path
 
 import tiktoken
 
 
 # Load the environment variables if working in weird environments
-def load_from_env_file():
+def load_from_env_file() -> None:
     """Load environment variables from a file."""
     # Load environment variables from project root .env file
-    # Get the current file's directory
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    # Get the parent directory of the current file's directory
-    parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
     # Set path of .env file
-    env_path = os.path.join(parent_dir, ".env")
-    try:
+    env_path = Path(__file__).parent.parent.joinpath(".env")
+    with contextlib.suppress(FileNotFoundError):
         # Load environment variables from .env file
         load_env_vars(env_path)
-    except FileNotFoundError:
-        pass
 
 
-def load_env_vars(path):
+def load_env_vars(path: Path) -> None:
     """Load environment variables from a file."""
-    with open(path) as f:
+    with Path.open(path) as f:
         for line in f:
             if "=" in line and not line.startswith("#"):
                 # Split the line into the variable name and value
@@ -40,41 +38,21 @@ def load_env_vars(path):
 
 
 # OpenAI method for counting tokens
-def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301"):
-    """Returns the number of tokens used by a list of messages."""
+def num_tokens_from_messages(messages: list[dict], model: str = "gpt-3.5-turbo-0301") -> int:
+    """Return the number of tokens used by a list of messages."""
     try:
         encoding = tiktoken.encoding_for_model(model)
     except KeyError:
         encoding = tiktoken.get_encoding("cl100k_base")
-    if (
-        model == "gpt-3.5-turbo-0301"
-    ):  # note: future models may deviate from this
+    if model == "gpt-3.5-turbo-0301":  # note: future models may deviate from this
         num_tokens = 0
         for message in messages:
             num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
             for key, value in message.items():
                 num_tokens += len(encoding.encode(value))
                 if key == "name":  # if there's a name, the role is omitted
-                    num_tokens += (
-                        -1
-                    )  # role is always required and always 1 token
+                    num_tokens += -1  # role is always required and always 1 token
         num_tokens += 2  # every reply is primed with <im_start>assistant
-        return num_tokens
     else:
-        raise NotImplementedError(
-            f"""num_tokens_from_messages() is not presently implemented for model {model}.
-  See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""",
-        )
-
-
-def read_version_from_file():
-    """Read the version from a file."""
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    # Get the parent directory of the current file's directory
-    parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
-    # Set path of version file
-    filename = os.path.join(parent_dir, "version.txt")
-    with open(filename) as file:
-        # Read the first line and strip any newline characters or spaces
-        version = file.readline().strip()
-    return version
+        raise NotImplementedError
+    return num_tokens
